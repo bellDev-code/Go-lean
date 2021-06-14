f3f799e3a5048af884898a592093e127860d7c84
bellDev-code <sumaoo20@naver.com>
2021-06-14 15:16:51 +0900

    packages and import

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	something "github.com/bellDev-code/leango/someting"
+)
+
+func main() {
+	fmt.Println("Hello world!!")
+	something.SayHello()
+	something.SayBye()
+}
