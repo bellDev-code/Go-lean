cf5d7db5a53592b8e8ad9270f64271a819c85665
bellDev-code <sumaoo20@naver.com>
2021-06-15 16:15:26 +0900

    nomade Mini project, account & newAccount

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
+	"github.com/bellDev-code/leango/accounts"
+)
 
 func main() {
-	array := map[string]string{"name":"lee", "age":"27"}
-	for key, value := range array {
-		fmt.Println(key, value)
-	}
-	
+	account := accounts.NewAccount("jongho")
+	fmt.Println(account)
 }
